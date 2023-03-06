17183da7555aeb052ebbbac1b617a3c3d2d04dfa
Tim Best <TimBest@users.noreply.github.com>
2023-03-06 13:25:26 -0500

    add todos

--- kit-api/index.js
+++ kit-api/index.js
@@ -10,7 +10,12 @@ app.listen(PORT, () => {
 });
 
 /// GET http://localhost:3001/api?kit_id=123
+// TODO: improve route name
 app.get("/api", (req, res) => {
+  // TODO: return error if kit_id is too small?
+  // TODO: return error if kit_id is too big?
+  // TODO: return error if kit_id contains anything other than a 0->9 or "-". safe assumption?
+  // TODO: order results by kit_id
   const c = db
     .prepare(
       `SELECT * FROM kits WHERE label_id LIKE '${req.query.kit_id}%' LIMIT 5;`
